3e8b89e61dc6b1cf596138e73b5112552e848a54
natesway <64743334+natesway@users.noreply.github.com>
2022-12-11 23:46:08 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ wget https://le-builds.lakka.tv/Generic.x86_64/Lakka-Generic.x86_64-4.2.img.gz
 gzip -d Lakka-Generic.x86_64-4.2.img.gz
 ls
 mkdir /tmp/lakka
+file Lakka-Generic.x86_64-4.2.img
+fdisk -lu Lakka-Generic.x86_64-4.2.img
 mount -o loop Lakka-Generic.x86_64-4.2.img /tmp/lakka/
 cd /tmp/lakka
 ls -l
