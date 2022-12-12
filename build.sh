1e0137cd5442d44c5716e2a54328f160fbaab0e9
natesway <64743334+natesway@users.noreply.github.com>
2022-12-12 00:00:23 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@ mkdir /tmp/lakka
 file Lakka-Generic.x86_64-4.2.img
 sudo apt install p7zip-full
 7z x Lakka-Generic.x86_64-4.2.img
+mkdir img
+mv system.img img
+cd img
+7z x system.img
 ls -l
 lsblk
